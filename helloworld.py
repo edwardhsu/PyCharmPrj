d30c5b1907da0668edf34406d0e62d5934f1766d
edwardhsu <edwardcy.hsu@gmail.com>
2017-04-25 15:13:48 +0800

    Hello World - A first .py written using PyCharm
    First program for the configuration of pyCharm on my PC using Window 8.1. As of 20-Apr-2017.

---
+++ helloworld.py
@@ -0,0 +1,7 @@
+age = int(input("Please enter your age: "))
+if (age <= 12) :
+    print("You are a kid!")
+elif ((age > 12)and(age < 20)) :
+    print("You are a teenager!")
+else :
+    print("You are a grown up!")
