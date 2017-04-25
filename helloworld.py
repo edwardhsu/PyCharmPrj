0e4ba069aebfe98b03f98296ad10d8b250162d9a
edwardhsu <edwardcy.hsu@gmail.com>
2017-04-25 16:58:54 +0800

    Updated by Edward. Simplify the code and adhere to PEP-8 Coding convention.

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,10 @@
+# Program is to determine whether a person is a boy,
+# or a teeanger or a man based on his age.
+
 age = int(input("Please enter your age: "))
-if (age <= 12) :
+if (age <= 12):
     print("You are a kid!")
-elif ((age > 12)and(age < 20)) :
+elif ((age > 12)and(age < 20)):
     print("You are a teenager!")
-else :
+else:
     print("You are a grown up!")
